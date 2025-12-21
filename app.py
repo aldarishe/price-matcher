8b3e95589a1635b17a1a7e06db177d6e73c08e5c
aldarishe <aldar.ayusheev@gmail.com>
2025-12-21 14:07:36 +0800

    Update app.py

--- app.py
+++ app.py
@@ -71,7 +71,7 @@ def find_best_column(df, target_type):
     return None
 
 def process_files(file1, file2, threshold):
-    df1 = pd.read_excel(file1)
+    df1 = pd.read_excel(file1; "наш прайс")
     df2 = pd.read_excel(file2)
     
     name_col1 = find_best_column(df1, 'name')
@@ -177,6 +177,7 @@ if file1 and file2:
             st.download_button("Скачать", b.getvalue(), "result_checked.xlsx")
         else:
             st.warning("Ничего не найдено.")
+
 
 
 
